7b6b40610b58eb98eb3456391621c2956e6d3e61
youngeiya <105629961+youngeiya@users.noreply.github.com>
2026-02-11 22:14:49 +0900

    super() this() 연습

--- java/basic/superthis.java
+++ java/basic/superthis.java
@@ -1,18 +1,20 @@
-class A{
-  A(String v){System.out.println(100);}
+class A {
+    A(String v) { System.out.println(100); }
 }
-class extends A{
-  B(String v){
-    super(v);
-    System.out.println(200);
-  }
-  B(int v){
-    this("aa");
-    System.out.println(300);
-  }
+
+class B extends A {
+    B(String v) {
+        super(v);
+        System.out.println(200);
+    }
+    B(int v) {
+        this("aa");
+        System.out.println(300);
+    }
 }
-class Main{
-  public static void main(String[] args){
-    B = o new B(400);
-  }
+
+public class Main {
+    public static void main(String[] args) {
+        B o = new B(400); 
+    }
 }
